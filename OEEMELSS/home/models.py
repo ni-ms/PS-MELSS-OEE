a86e0dad723e5d2fa7f8a8cc7dac36b798e0d946
AbhirathNB <f20200260@hyderabad.bits-pilani.ac.in>
2022-06-30 20:27:27 +0530

    update 5

--- OEEMELSS/home/models.py
+++ OEEMELSS/home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class OEEValues(models.Model):
+    partid = models.CharField(max_length=255)
+    machineid = models.CharField(max_length=255)
+    oeevalue = models.FloatField()
+    Avalue = models.FloatField()
+    Pvalue = models.FloatField()
+    Qvalue = models.FloatField()
