e76a878fc8969ef361fbcf51fa5c339c6ed53b24
AbhirathNB <f20200260@hyderabad.bits-pilani.ac.in>
2022-06-30 10:38:29 +0530

    update 30/6/22 2

--- OEEMELSS/OEEMELSS/settings.py
+++ OEEMELSS/OEEMELSS/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'OEEMELSS',
+    'home',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'OEEMELSS.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "static", BASE_DIR / "template"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +123,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# added manually
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
