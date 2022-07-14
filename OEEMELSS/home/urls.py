b639994b0302b5e71d450f1618b9e48bf595fb4e
Prod23 <f20202075@hyderabad.bits-pilani.ac.in>
2022-07-14 11:53:01 +0530

    changes in frontend part

--- OEEMELSS/home/urls.py
+++ OEEMELSS/home/urls.py
@@ -3,9 +3,14 @@ from django.urls import path
 from home import views
 
 urlpatterns = [
+    path("admin/",admin.site.urls),
+    path("dashboard/", views.dashboard, name = 'dashboard'),
+    path("logout/", views.logoutPage, name = 'logout'),
+    path("loginError/", views.loginErrorPage, name = 'loginError'),
+    path("errorPage/", views.ErrorPage, name = '404error'),
     path("", views.entryPage, name = 'EntryPage'),
-    path("inputValues", views.inputValues, name = 'InputValues'),
-    path("displayPage", views.displayPage, name = 'DisplayPage'),
-    path("getHistoricalData", views.getHistoricalData, name = 'GetHistoricalData'),
-    path("displayHistoricalData", views.displayHistoricalData, name = 'DisplayhistoricalData'),
+    path("inputValues/", views.inputValues, name = 'inputValues'),
+    path("displayPage/", views.displayPage, name = 'displayPage'),
+    path("getHistoricalData/", views.getHistoricalData, name = 'getHistoricalData'),
+    path("displayHistoricalData/", views.displayHistoricalData, name = 'displayHistoricalData'),
 ]
--- OEEMELSS/home/views.py
+++ OEEMELSS/home/views.py
@@ -9,14 +9,26 @@ from django.contrib import messages
 def entryPage(request):
     return render(request, 'LandingPage.html') #HttpResponse("this is homepage, or login") 
 
+def dashboard(request):
+    return render(request, 'Pages/HomePage.html')
+
+def logoutPage(request):
+    return render(request,'Pages/LogoutPage.html')
+
+def loginErrorPage(request):
+    return render(request,'Pages/401Page.html')
+
+def ErrorPage(request):
+    return render(request,'Pages/404Page.html')
+
 def inputValues(request):
-    return render(request, 'CalculateOEE.html') #HttpResponse("this is where you input values")
+    return render(request, 'Pages/CalculateOEE.html') #HttpResponse("this is where you input values")
 
 def displayPage(request):
-    return render(request, 'OEEOutput.html') #HttpResponse("this is where you show OEE value")
+    return render(request, 'Pages/OEEOutput.html') #HttpResponse("this is where you show OEE value")
 
 def getHistoricalData(request):
-    return render(request, 'DisplayOEE.html') #HttpResponse("this is where you input machine and part id to display historical OEE value")
+    return render(request, 'Pages/DisplayOEE.html') #HttpResponse("this is where you input machine and part id to display historical OEE value")
 
 def displayHistoricalData(request):
-    return render(request, 'OEEGraph.html') #HttpResponse("this is where you show historical OEE value for a given machine and part id")
+    return render(request, 'Pages/OEEGraph.html') #HttpResponse("this is where you show historical OEE value for a given machine and part id")
