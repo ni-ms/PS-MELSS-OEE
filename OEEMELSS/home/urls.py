784bb238107f793ed07771b8e438181f2499c6b1
Prod23 <f20202075@hyderabad.bits-pilani.ac.in>
2022-07-15 12:50:20 +0530

    Update urls.py

--- OEEMELSS/home/urls.py
+++ OEEMELSS/home/urls.py
@@ -4,13 +4,22 @@ from home import views
 
 urlpatterns = [
     path("admin/",admin.site.urls),
+
     path("dashboard/", views.dashboard, name = 'dashboard'),
+
     path("logout/", views.logoutPage, name = 'logout'),
+
     path("loginError/", views.loginErrorPage, name = 'loginError'),
+
     path("errorPage/", views.ErrorPage, name = '404error'),
+
     path("", views.entryPage, name = 'EntryPage'),
+
     path("inputValues/", views.inputValues, name = 'inputValues'),
+
     path("displayPage/", views.displayPage, name = 'displayPage'),
+
     path("getHistoricalData/", views.getHistoricalData, name = 'getHistoricalData'),
+    
     path("displayHistoricalData/", views.displayHistoricalData, name = 'displayHistoricalData'),
 ]
